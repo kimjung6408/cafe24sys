8c9d683fb9eb5923dcf785f36984b805753a248a
unknown <kimjung6408@gmail.com>
2023-12-19 14:53:43 +0900

    first

---
+++ src/main/java/cafe24customer/customersys/CustomersysApplication.java
@@ -0,0 +1,13 @@
+package cafe24customer.customersys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomersysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomersysApplication.class, args);
+	}
+
+}
